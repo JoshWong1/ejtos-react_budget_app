648737e17664811c52d2469c279753df3f86cd50
Josh <wongj09@gmail.com>
2023-04-14 16:53:17 -0400

    changed buttons, fixed bugs

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import {FaPlusCircle, FaMinusCircle} from 'react-icons/fa';
 
 const ExpenseItem = (props) => {
     const { dispatch, currency } = useContext(AppContext);
@@ -42,8 +43,8 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button size='1.5em' onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
+        <td><FaPlusCircle size='1.5em' color="green" onClick={event=> increaseAllocation(props.name)}>+</FaPlusCircle></td>
+        <td><FaMinusCircle size='1.5em' color="red" onClick={event=> decreaseAllocation(props.name)}>-</FaMinusCircle></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
